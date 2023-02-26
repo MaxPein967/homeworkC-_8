130b68c3572fb5952c13c8b5e333b77cddaf683e
Sergei Vasiliev <9670105747@mail.ru>
2023-02-26 12:15:48 +0700

    Task 62

--- C#_62/Program.cs
+++ C#_62/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+Console.Clear();
+
+int[,] nums = new int[4, 4];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= nums.GetLength(0) * nums.GetLength(1))
+{
+    nums[i, j] = temp;
+    temp++;
+    if (i <= j + 1 && i + j < nums.GetLength(1) - 1)
+        j++;
+    else if (i < j && i + j >= nums.GetLength(0) - 1)
+        i++;
+    else if (i >= j && i + j > nums.GetLength(1) - 1)
+        j--;
+    else
+        i--;
+}
+
+PrintArray(nums);
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < 10)
+            {
+                Console.Write("0" + array[i, j]);
+                Console.Write(" ");
+            }
+            else Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
